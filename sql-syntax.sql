b0d0fbc6fdb37e77e22422bc46a2c01ac2eaccb9
arslan <arslanash@gmail.com>
2018-12-28 15:38:26 -0500

    syntax added

--- sql-syntax.sql
+++ sql-syntax.sql
@@ -1,1 +1,30 @@
+-- create table
+	create table table_name_foo (
+		id int not null primary key auto_increment,
+		firstname char(255),
+		last_name varchar(255),
+		info text,
+		created_at DATE,  -- yyyy-mm-dd
+		updated_at DATETIME,  -- yyyy-mm-dd hh:mm:ss
+		bar_id int,
+		foreign key (bar_id) references table_name_bar(id)
+		-- PRIMARY KEY (id) also acceptable 
+		-- contraint PK_table_name_foo PRIMARY KEY (id) => in sql server
+		-- bar_id int foreign key references table_name_bar(id) => sql server
+	)
+
+-- insert into table
+	insert into table_name_foo values ()
+
+-- alter table
+	alter table table_name_foo 
+	drop 
+	primary key;  -- drop contraint PK_table_name_foo => in sql server
+
+	alter table table_name_foo 
+	add -- add contraint PK_table_name_foo => sql server
+	primary key (id);
+
+
+
 select * from table_name;
